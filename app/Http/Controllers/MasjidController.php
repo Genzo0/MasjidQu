37927a944257c4aa4c5fb2ab406f1f93b26d6281
genzo0 <m.alifyan02@gmail.com>
2022-11-19 14:07:40 +0700

    fix register redirect

--- app/Http/Controllers/MasjidController.php
+++ app/Http/Controllers/MasjidController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Masjid;
-use App\Models\PengurusMasjid;
 use App\Models\Kajian;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Gate;
