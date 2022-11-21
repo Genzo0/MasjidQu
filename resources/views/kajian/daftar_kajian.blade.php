56d462e4cca6de2c53e76e61e8b9303b1689cebe
inlandsche <qolandara@gmail.com>
2022-11-21 21:46:33 +0700

    edit page view

--- resources/views/kajian/daftar_kajian.blade.php
+++ resources/views/kajian/daftar_kajian.blade.php
@@ -71,12 +71,12 @@
                 </div>
               </div>
             </div>
-          </div>
         </div>
     @empty
     <div>
         <h1 class="position-absolute top-50 start-50 translate-middle fw">belum ada data kajian</h1>
     </div>
     @endforelse
+</div>
 </div>
 @endsection
